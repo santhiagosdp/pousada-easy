4bb32439701fdfdccc15a1e110b90db4f94b4de6
Santhiago Dionizio Pinto <santhiagosdp@gmail.com>
2023-03-03 16:36:22 -0300

    re
    ref

--- pousadaeasy/settings.py
+++ pousadaeasy/settings.py
@@ -117,3 +117,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' # ou outro servidor SMTP que você use
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '' # ou outro email que você use
+EMAIL_HOST_PASSWORD = '' # a senha do email que você use
